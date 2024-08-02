8d49e7c611e829416dda92efbf85d8dde618d243
martial-plains <41553768+martial-plains@users.noreply.github.com>
2024-08-02 10:56:25 -0400

    feat: implement requested feature for issue #18
    Closes #18

--- src/components.rs
+++ src/components.rs
@@ -82,6 +82,12 @@ pub fn statistics_options_panel() -> impl IntoView {
                                                 <span class="text-4xl text-black dark:text-white">{state.show_longest_sentence_words_count()}</span>
                                             </>
                                         },
+                                        StatisticOption::ShortestSentenceWords => view! {
+                                            <>
+                                                <div class="uppercase text-xs">{"Shortest Sentence (Words)"}</div>
+                                                <span class="text-4xl text-black dark:text-white">{state.show_shortest_sentence_words_count()}</span>
+                                            </>
+                                        },
                                         StatisticOption::SpeakingTime => view! {
                                             <>
                                                 <div class="uppercase text-xs whitespace-nowrap">
